0e7251ddd189565d885109c0a7f662f5e85111c4
harshdev2909 <harsh.dev2909@gmail.com>
2024-08-18 07:49:06 +0530

    added

--- assets/js/script.js
+++ assets/js/script.js
@@ -52,12 +52,13 @@ const backTopBtn = document.querySelector("[data-back-top-btn]");
 
 const headerActive = function () {
   if (window.scrollY > 80) {
-    header.classList.add("active");
-    backTopBtn.classList.add("active");
-  } else {
     header.classList.remove("active");
     backTopBtn.classList.remove("active");
+  } else {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
   }
 }
+
 
 addEventOnElem(window, "scroll", headerActive);
